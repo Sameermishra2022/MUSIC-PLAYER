1601ea11c0d17239243f41ba016de163b9b11acc
Sameer Mishra <115838087+Sameermishra2022@users.noreply.github.com>
2023-10-18 09:57:43 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,35 @@
+let allMusic = [
+{
+    name: "Dagabaaz-Re",
+    artist: "Shreya Ghoshal",
+    img: "m1",
+    src: "m1"
+},
+{
+    name: "Surili Akhiyon Wale",
+    artist: "Rahat Fateh Ali Khan",
+    img: "m2",
+    src: "m2"
+},
+
+{
+    name: "Swag Se Swagat",
+    artist: "Neha Bhasin",
+    img: "m3",
+    src: "m3"
+},
+
+{
+    name: "TERE NAAM",
+    artist: "Udit Narayan",
+    img: "m4",
+    src: "m4"
+},
+
+{
+    name: "Tere-Mast-Mast-Do-Nain",
+    artist: "Shreya Ghoshal",
+    img: "m5",
+    src: "m5"
+},
+];
